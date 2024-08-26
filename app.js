c1c1b03b118dd1ec570d52d6f6790b9ef310f3a0
aryani24 <mrflatboy@gmail.com>
2024-08-26 19:25:59 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+let items = document.querySelectorAll('.slider .list .item');
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+let lastPosition = items.length - 1;
+let firstPosition = 0;
+let active = 0;
+
+let music_kontrol = document.getElementById('music_control');
+let music_player = document.getElementById('music_player');
+let isPlay = false;
+
+function mulai() {
+    if (isPlay) {
+        music_player.pause();
+        isPlay = false;
+        music_kontrol.innerHTML = 'PLAY music';
+    } else {
+        music_player.play();
+        isPlay = true;
+        music_kontrol.innerHTML = 'STOP music';
+    }
+}
+
+
+nextBtn.onclick = () => {
+    active = active + 1;
+    setSlider();
+}
+prevBtn.onclick = () => {
+    active = active - 1;
+    setSlider();
+}
+const setSlider = () => {
+    let oldActive = document.querySelector('.slider .list .item.active');
+    if(oldActive) oldActive.classList.remove('active');
+    items[active].classList.add('active');
+    // 
+    nextBtn.classList.remove('d-none');
+    prevBtn.classList.remove('d-none');
+    if(active == lastPosition) nextBtn.classList.add('d-none');
+    if(active == firstPosition) prevBtn.classList.add('d-none');
+}
+setSlider();
+
+// set diameter
+const setDiameter = () => {
+    let slider = document.querySelector('.slider');
+    let widthSlider = slider.offsetWidth;
+    let heightSlider = slider.offsetHeight;
+    let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2));
+    document.documentElement.style.setProperty('--diameter', diameter+'px');
+}
+setDiameter();
+window.addEventListener('resize', () => {
+    setDiameter();
+});
+
